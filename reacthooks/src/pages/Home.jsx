c5a5e75780a076f82212d5586c5a0fc5d0cb0d0b
Michael Almeida <michael.almeida@fundatec.org.br>
2024-07-21 21:44:56 -0300

    useReducer

--- reacthooks/src/pages/Home.jsx
+++ reacthooks/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HookUseState from "../components/HookUseState";
+import HookuseReducer from "../components/HookuseReducer";
 
 const Home = () => {
   return (
     <div>
       <div>
         <HookUseState />
+        <HookuseReducer />
       </div>
     </div>
   );
